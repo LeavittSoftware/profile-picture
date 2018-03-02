9f110a83ede166b0efac94893b27ccda9270e2d8
Aaron Drabeck <github@aarondrabeck.com>
2018-03-02 14:19:00 -0700

    Adds user manager to profile-picture-menu element.

--- types/lss-profile-picture-menu.d.ts
+++ types/lss-profile-picture-menu.d.ts
@@ -1,8 +1,13 @@
 declare const LssProfilePictureMenu_base: Polymer.LazyImportsMixinConstructor & typeof Polymer.Element & Polymer.GestureEventListenersConstructor;
 declare class LssProfilePictureMenu extends LssProfilePictureMenu_base {
     personId: number;
+    roles: Array<string>;
     fullname: string;
-    dialog: any;
+    disableAutoload: boolean;
+    dialog: PaperDialogElement;
+    userManager: LssUserManager;
+    profilePicture: LSSProfilePicture;
+    innerProfilePicture: LSSProfilePicture;
     refresh(): void;
     protected _onProfilePictureTapped(): Promise<void>;
     protected _onMyAccountTapped(): void;
